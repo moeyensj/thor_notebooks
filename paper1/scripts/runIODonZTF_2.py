d3d6805bd7c071d3af47123067bce47032c70b60
Joachim Moeyens <moeyensj@gmail.com>
2020-01-17 12:14:57 -0800

    Add chunk based processing of orbits

--- paper1/scripts/runIODonZTF_2.py
+++ paper1/scripts/runIODonZTF_2.py
@@ -1,5 +1,7 @@
-RUN_DIR = "/Users/moeyensj/Google Drive/Astronomy/Data/thor/results/ztf/run_16/"
-THOR_DIR = "/Users/moeyensj/projects/thor/thor"
+RUN_DIR = "/gscratch/astro/moeyensj/projects/thor/paper1/results/ztf/run_16"
+THOR_DIR = "/gscratch/astro/moeyensj/projects/thor/thor"
+#RUN_DIR = "/Users/moeyensj/Google Drive/Astronomy/Data/thor/results/ztf/run_16/"
+#THOR_DIR = "/Users/moeyensj/projects/thor/thor"
 
 import os
 import sys
@@ -16,7 +18,9 @@ from thor.constants import Constants as c
 from thor.orbits.iod import iod
 
 MU = c.G * c.M_SUN
-THREADS = 28
+THREADS = mp.cpu_count()
+os.environ["OMP_NUM_THREADS"] = "1"
+np.seterr("ignore")
 
 def _iod(obs_ids,
          cluster_id,
@@ -47,9 +51,9 @@ def _iod(obs_ids,
     
     if orbit is not None:
         orbit_id = "{}".format("_".join(best_obs_ids.astype(str)))
-        return [orbit_id, cluster_id, *orbit[:], min_chi2]
+        return np.array([orbit_id, cluster_id, *orbit[:], min_chi2])
     else:
-        return []
+        return np.array([np.NaN for i in range(10)])
 
 def initialOrbitDetermination(observations, 
                               clusterMembers, 
@@ -59,7 +63,7 @@ def initialOrbitDetermination(observations,
                               max_iter=50,
                               tol=1e-15, 
                               propagatorKwargs={
-                                "observatoryCode" : "I41",
+                                "observatoryCode" : "I11",
                                 "mjdScale" : "UTC",
                                 "dynamical_model" : "2",
                                },
@@ -68,30 +72,43 @@ def initialOrbitDetermination(observations,
                               columnMapping=Config.columnMapping):
 
     grouped = clusterMembers.groupby(by="cluster_id")[columnMapping["obs_id"]].apply(list)
-    cluster_ids = grouped.index.values
-    obs_ids = grouped.values
-
-    orbits = []
-    if threads > 1:
-        p = mp.Pool(threads)
-        orbits = p.starmap(partial(_iod,
-                                   observations=observations,
-                                   observation_selection_method=observation_selection_method,
-                                   iterate=iterate, 
-                                   light_time=light_time,
-                                   max_iter=max_iter, 
-                                   tol=tol, 
-                                   propagatorKwargs=propagatorKwargs,
-                                   mu=mu, 
-                                   columnMapping=columnMapping),
-                           zip(obs_ids, cluster_ids))
-
-    orbits_df = pd.DataFrame(orbits, columns=["orbit_id", "cluster_id", "epoch_mjd", "obj_x", "obj_y", "obj_z", "obj_vx", "obj_vy", "obj_vz", "chi2"])
+    cluster_ids = list(grouped.index.values)
+    obs_ids = grouped.values.tolist()
     
-    # Remove empty rows
+    chunk_size = 1000
+    num_chunks = int(np.round(len(cluster_ids) / chunk_size))
+    if num_chunks == 0 and len(cluster_ids) != 0:
+        num_chunks = 1
+
+    orbits_dfs = []
+    if threads > 1:
+        for chunk in range(num_chunks):
+            p = mp.Pool(threads)
+            orbits = p.starmap(partial(_iod,
+                                       observations=observations,
+                                       observation_selection_method=observation_selection_method,
+                                       iterate=iterate, 
+                                       light_time=light_time,
+                                       max_iter=max_iter, 
+                                       tol=tol, 
+                                       propagatorKwargs=propagatorKwargs,
+                                       mu=mu, 
+                                       columnMapping=columnMapping),
+                               zip(obs_ids[chunk * chunk_size : chunk * chunk_size + chunk_size], 
+                                   cluster_ids[chunk * chunk_size : chunk * chunk_size + chunk_size]))
+            
+            orbits_df = pd.DataFrame(orbits, columns=["orbit_id", "cluster_id", "epoch_mjd", "obj_x", "obj_y", "obj_z", "obj_vx", "obj_vy", "obj_vz", "chi2"])
+            orbits_dfs.append(orbits_df)
+
+    
+    orbits_df = pd.concat(orbits_dfs)
     orbits_df.dropna(inplace=True)
-    return orbits_df
-                                   
+    orbits_df.reset_index(inplace=True, drop=True)
+    orbits_df["cluster_id"] = orbits_df["cluster_id"].astype(int)
+    # Remove empty rows
+    
+    return orbits_df 
+
 
 columnMapping = {        
         
@@ -188,9 +205,9 @@ for orbit_id in test_orbits["orbit_id"].unique()[407:]:
 
     orbit_dir = os.path.join(RUN_DIR, "orbit_{:04d}".format(orbit_id))
     
-    allClusters = pd.read_csv(os.path.join(orbit_dir, "allClusters.txt"), sep=" ", index_col=False)
-    clusterMembers = pd.read_csv(os.path.join(orbit_dir, "clusterMembers.txt"), sep=" ", index_col=False)
-    projected_obs = pd.read_csv(os.path.join(orbit_dir, "projected_obs.txt"), sep=" ", index_col=False)
+    allClusters = pd.read_csv(os.path.join(orbit_dir, "allClusters.txt"), sep=" ", index_col=False, low_memory=False, dtype={"cluster_id": int, "linked_object" : str})
+    clusterMembers = pd.read_csv(os.path.join(orbit_dir, "clusterMembers.txt"), sep=" ", index_col=False, low_memory=False, dtype={"cluster_id": int})
+    projected_obs = pd.read_csv(os.path.join(orbit_dir, "projected_obs.txt"), sep=" ", index_col=False, low_memory=False)
     
     astrometric_err = 1/3600/10 # in degrees
     #projected_obs["RA_deg"] = projected_obs["RA_deg"] + np.random.normal(loc=0, scale=astrometric_err, size=len(projected_obs)) * np.cos(np.radians(projected_obs["Dec_deg"].values))
@@ -222,7 +239,6 @@ for orbit_id in test_orbits["orbit_id"].unique()[407:]:
     iod_orbits = iod_orbits.merge(allClusters[["cluster_id", "linked_object", "pure", "partial", "false"]], on="cluster_id")
     iod_orbits["orbit_id"] = [orbit_id for i in range(len(iod_orbits))]
     iod_orbits["linked_object"] = iod_orbits["linked_object"].astype(str)
-    iod_orbits["cluster_id"] = iod_orbits["cluster_id"].astype(int)
     
     iod_orbits.to_csv(os.path.join(IOD_DIR, "iod_orbit_{:04d}.txt".format(orbit_id)), sep=" ", index=False)
     
@@ -230,4 +246,4 @@ for orbit_id in test_orbits["orbit_id"].unique()[407:]:
     print("")
 
 print("Total time: {}".format(time.time() - time_start))
-    
+print("JOB Complete")
